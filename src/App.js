8421146e970d103ed7afa6333e43fdfb92b68e39
jiangyuelong <602908219@qq.com>
2022-04-14 18:08:49 +0800

    导出webcomponent组件

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+/*
+ * @Author: 姜跃龙
+ * @Date: 2022-04-13 21:19:06
+ * @LastEditors: 姜跃龙
+ * @LastEditTime: 2022-04-13 22:23:31
+ * @Description: file content
+ */
+import React, { useState } from 'react';
+import { TreeSelect } from 'antd';
 
-function App() {
+const { TreeNode } = TreeSelect;
+
+const Demo = () => {
+  const [value, setValue] = useState(undefined);
+  const onChange = () => {
+    setValue(value);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <TreeSelect
+      showSearch
+      style={{ width: '100%' }}
+      value={value}
+      dropdownStyle={{ maxHeight: 400, overflow: 'auto' }}
+      placeholder="Please select"
+      allowClear
+      treeDefaultExpandAll
+      onChange={onChange}
+    >
+      <TreeNode value="parent 1" title="parent 1">
+        <TreeNode value="parent 1-0" title="parent 1-0">
+          <TreeNode value="leaf1" title="leaf1" />
+          <TreeNode value="leaf2" title="leaf2" />
+        </TreeNode>
+        <TreeNode value="parent 1-1" title="parent 1-1">
+          <TreeNode value="leaf3" title={<b style={{ color: '#08c' }}>leaf3</b>} />
+        </TreeNode>
+      </TreeNode>
+    </TreeSelect>
   );
-}
+};
 
-export default App;
+
+export default Demo;
